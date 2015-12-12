f5adbdb09da37a55b8dbe503916dfab7b20b1e8f
Boblebol <alexandre.enouf@gmail.com>
2015-12-12 18:16:08 +0100

    Update test.c

--- test.c
+++ test.c
@@ -1,20 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "symbol.h"
-#include "quad.h"
-	
-int main(){
- 
-  struct symbol* tds = NULL;	
-  struct quad* code = NULL;
-  
-  struct symbol* un = symbol_newtemp(&tds);
-  un->value = 1;
-
-  printf("Table des symboles\n");
-  symbol_print(tds);
-  printf("\nCode\n");
-  quad_print(code);
-  return 0;
+int main() {
+	int a;	
+	a=1;
+	a=a-1;
 }
