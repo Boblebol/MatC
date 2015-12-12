67ae5519c10dc16b3c2811c9e3ca42a6d05bab3a
Boblebol <alexandre.enouf@gmail.com>
2015-12-12 18:13:39 +0100

    Update quad.c

--- quad.c
+++ quad.c
@@ -4,8 +4,18 @@
 #include "quad.h"
 #include "symbol.h"
 
-void quad_add(struct quad** list, struct quad* new) {
-    if (*list == NULL) {
+struct quad* quad_gen(char* op, struct symbol* arg1, struct symbol* arg2, struct symbol* res) {
+	struct quad* new = malloc(sizeof(*new));	
+    new->op = strdup(op);	
+    new->arg1 = arg1;
+    new->arg2 = arg2;
+    new->res = res;
+    new->next = NULL;
+    return new;
+}
+
+void quad_add(struct quad** list, struct quad* new) {    
+	if (*list == NULL) {
         *list = new;
     } else {
         struct quad* scan = *list;
@@ -16,19 +26,19 @@ void quad_add(struct quad** list, struct quad* new) {
     }
 }
 
-struct quad* quad_gen(char op, struct symbol* arg1, struct symbol* arg2, struct symbol* res) {
-    struct quad* new = malloc(sizeof(*new));
-    new->op = op;
-    new->arg1 = arg1;
-    new->arg2 = arg2;
-    new->res = res;
-    new->next = NULL;
-    return new;
+void quad_print(struct quad* list) {
+	while (list != NULL) {
+        printf("%s %7s %7s %7s\n", list->op, list->arg1->id, list->arg2->id, list->res->id);
+        list = list->next;
+    }
 }
 
-void quad_print(struct quad* list) {
-    while (list != NULL) {
-        printf("op : %c, arg1 : %s, arg2 : %s result : %s\n", list->op, list->arg1->id, list->arg2->id, list->res->id);
-        list = list->next;
+void quad_free(struct quad* list) {
+    if (list != NULL) {
+        if (list->next != NULL) {
+            quad_free(list->next);
+        }
+        free(list);
+        list = NULL;
     }
 }
