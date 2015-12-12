315b34d888972bde33906507734f51d990a6e646
Boblebol <alexandre.enouf@gmail.com>
2015-12-12 18:14:27 +0100

    Update quad.h

--- quad.h
+++ quad.h
@@ -1,11 +1,17 @@
+#ifndef QUAD_H_
+#define QUAD_H_
+
 struct quad {
-    char op;
+    char* op;
     struct symbol* arg1;
     struct symbol* arg2;
     struct symbol* res;
     struct quad* next;
 };
 
-void quad_add(struct quad** list, struct quad* new);
-struct quad* quad_gen(char op, struct symbol* arg1, struct symbol* arg2, struct symbol* res);
-void quad_print(struct quad* list);
+struct quad*    quad_gen(char* op, struct symbol* arg1, struct symbol* arg2, struct symbol* res);
+void            quad_free(struct quad*);
+void            quad_add(struct quad** list, struct quad* new);
+void            quad_print(struct quad* list);
+
+#endif
