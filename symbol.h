95dcbe6c039e9e5b922c38dcba89f1c54a50b6d2
Boblebol <alexandre.enouf@gmail.com>
2015-12-12 18:15:25 +0100

    Update symbol.h

--- symbol.h
+++ symbol.h
@@ -1,14 +1,30 @@
+#ifndef SYMBOL_H_
+#define SYMBOL_H_
+
 #include <stdbool.h>
 #define SYMBOL_MAX_NAME 64
+
+enum symbol_types {
+    SYMBOL_INT,
+    SYMBOL_FLOAT
+};
 
 struct symbol {
     char* id;
     bool isconstant;
-    int value;
+    enum symbol_types stype;
+    union {
+        int int_val;
+        float float_val;
+    } v;
     struct symbol* next;
 };
 
 struct symbol* symbol_alloc();
-struct symbol* symbol_newtemp(struct symbol** tds);
-struct symbol* symbol_add(struct symbol** tds, char* id);
-void symbol_print(struct symbol* tds);
+struct symbol* symbol_newtemp(struct symbol**);
+struct symbol* symbol_add(struct symbol**, char*);
+struct symbol* symbol_lookup(struct symbol*, char*);
+void           symbol_free(struct symbol*);
+void           symbol_print(struct symbol*);
+
+#endif
