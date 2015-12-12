4af4fef0e513fd236c292be4d05e304d99ac4b8c
Boblebol <alexandre.enouf@gmail.com>
2015-12-12 20:02:33 +0100

    Update test.c

--- test.c
+++ test.c
@@ -1,5 +1,13 @@
 int main() {
-	int a;	
-	a=1;
-	a=a-1;
+	int a ;
+	int b ;	
+	a = 1 ;
+	b = 212 ;
+	a = a - 1 + 688888 ;  
+	b = a + 1 ;
+	b = b + a ;
+	b = -a ;
+	b = b * 2 ;
+	a = b / 2 ;
+	return 0;
 }
